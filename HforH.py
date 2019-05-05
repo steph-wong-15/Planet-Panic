d118c9c76ac172ed13ea3d505f84732dc4ea4f4b
steph-wong-15 <50210628+steph-wong-15@users.noreply.github.com>
2019-05-05 14:36:33 -0700

    Update HforH.py

--- HforH.py
+++ HforH.py
@@ -1,7 +1,6 @@
 import turtle
 import time
 import random
-
 ##INTRO
 
 print("Welcome to Planet Panic! You will be assigned an object that you must collect, it will be either garbage, recycling, or compost. Your mission is to collect the targets to gain points!")
@@ -65,14 +64,12 @@ head.direction = "stop"
 
 # Snake targ
 targ = turtle.Turtle()
-targ.shape("square")
 targ.speed(0)
+targ.shape("square")
 targ.color(color)
 targ.penup()
 targ.goto(100,50)
 
-
-    
 # Snake obs1
 obs1 = turtle.Turtle()
 obs1.speed(0)
@@ -115,7 +112,129 @@ def go_left():
         head.direction = "left"
 
 def go_right():
- 
+    if head.direction != "left":
+        head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
+
+pen.goto(0,100)
+pen.write("Welcome to Planet Panic", align="center", font=("Courier", 36, "bold"))
+pen.goto(0,0)
+    
+# Main game loop
+while True:
+    wn.update()
+    
+    # Check for a collision with the border
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+        # Hide the segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+        
+        # Clear the segments list
+        segments.clear()
+
+        # Reset the score
+        score = 0
+
+        # Reset the delay
+        delay = 0.1
+
+        pen.clear()
+        pen.write("Collected: {}  Most Collected: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+
+
+    # Check for a collision with the food
+    if head.distance(targ) < 20:
+        # Move the food to a random spot
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        targ.goto(x,y)
+        obs1.goto(random.randint(-290, 290),random.randint(-290, 290))
+        obs2.goto(random.randint(-290, 290),random.randint(-290, 290))
+
+        # Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color(color)
+        new_segment.penup()
+        segments.append(new_segment)
+
+        # Shorten the delay
+        delay -= 0.001
+
+        # Increase the score
+        score += 1
+
+        if score > high_score:
+            high_score = score
+        
+        pen.clear()
+        pen.write("Collected: {}  Most Collected: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+
+    if head.distance(obs1)<20 or head.distance(obs2)<20:
+        score=score-1
+        obs1.goto(random.randint(-290, 290),random.randint(-290, 290))
+        obs2.goto(random.randint(-290, 290),random.randint(-290, 290))
+        pen.clear()
+        pen.write("Collected: {}  Most Collected: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+        
+    # Move the end segments first in reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
+    move()    
+
+    # Check for head collision with the body segments
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+        
+            # Hide the segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+        
+            # Clear the segments list
+            segments.clear()
+
+            # Reset the score
             score = 0
 
             # Reset the delay
